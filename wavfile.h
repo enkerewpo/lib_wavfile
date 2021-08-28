882f4665cd9070d99fb15fa7792aaf7ae5bdec60
Kvar_ispw17 <enkerewpo@gmail.com>
2021-08-28 17:50:19 +0800

    vs
    vs

--- wavfile.h
+++ wavfile.h
@@ -11,8 +11,13 @@ http://www.nd.edu/~dthain/courses/cse20211/fall2013/wavfile
 #ifndef WAVFILE_H
 #define WAVFILE_H
 
+#pragma warning(disable : 4996)
+
 #include <stdio.h>
 #include <inttypes.h>
+#include <cmath>
+
+const double M_PI = acos(-1.);
 
 FILE * wavfile_open( const char *filename );
 void wavfile_write( FILE *file, short data[], int length );
